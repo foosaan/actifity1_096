473ad78e86280f927f32edc4ac2e8871a2c1dc5e
foosaan <fauzan4297@gmail.com>
2023-03-15 11:51:30 +0700

    membuat pemprogaman dengan kondisi ifalse

---
+++ kondisi_2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+double rerata(double a, double b){
+    return (a+b)/2;
+
+}
+string status (double c){
+    if (c >=60)
+    return "lulus";
+    else
+    return "gagal";
+}
+string status2 (double r, double n){
+    if (r >=60 && n >=70)
+    return "lulus";
+    else
+    return "gagal";
+}
+string status3 (double r, double n, double m){
+    if (r >=60 || n >=70 && m >=60)
+    return "lulus";
+    else
+    return "gagal";
+}
+int main(){
+    double nilM,nilB;
+
+    cout << "masukkan nilai matematika = ";
+    cin >> nilM;
+    cout << "masukkan nilai bahasa = ";
+    cin >> nilB;
+
+    cout<< "Status Kelulusan : " << status(rerata(nilM,nilB));
+    cout<< "\nStatus Kelulusan ke 2" << status2(rerata(nilM,nilB),nilM);
+    cout<< "\nStatus Kelulusan ke 3" << status3(rerata(nilM,nilB),nilM,nilB);
+    return 0;
+}
